477a238be05cf6ba81c099fc07abd71a67ffd11f
Andre Kraus <info@andrekraus.com>
2023-03-17 20:53:03 +0100

    [TASK] update example content blocks

--- ContentBlocks/accordion/Resources/Public/Frontend.js
+++ ContentBlocks/accordion/Resources/Public/Frontend.js
@@ -2,14 +2,14 @@ let acc = document.getElementsByClassName("accordion-item");
 let i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
 
     let panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
     } else {
-      panel.style.display = "block";
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
 }
